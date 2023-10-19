1cf54c88bf16a9459913a30283c5ba72ee2f812f
MylesGit <37464485+Sayinq@users.noreply.github.com>
2023-10-19 18:01:59 -0400

    Customization section initial

--- Landing/src/components/ImageCarousel.jsx
+++ Landing/src/components/ImageCarousel.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
 import '../index.css';
+import firstScreenshot from '../assets/Screenshots/FirstScreenshot.jpg';
+import secondScreenshot from '../assets/Screenshots/SecondScreenshot.jpg';
+import thirdScreenshot from '../assets/Screenshots/ThirdScreenshot.jpg';
+import fourthScreenshot from '../assets/Screenshots/FourthScreenshot.jpg';
+import fifthScreenshot from '../assets/Screenshots/FifthScreenshot.jpg';
+import sixthScreenshot from '../assets/Screenshots/SixthScreenshot.jpg';
+import seventhScreenshot from '../assets/Screenshots/SeventhScreenshot.jpg';
+import eighthScreenshot from '../assets/Screenshots/EighthScreenshot.jpg';
+import ninethScreenshot from '../assets/Screenshots/NinethScreenshot.jpg';
 
 const images = [
-  'https://cdn.midjourney.com/8ef1e3ab-8046-4770-8bc6-d399d5224682/0_0_384_N.webp',
-  'https://cdn.midjourney.com/ef274612-8bb6-4fc3-96c2-c4cc18a0b23e/0_0_384_N.webp',
-  'https://cdn.midjourney.com/ead86688-4486-4397-88e5-2d3db6ebb070/0_0.png',
-  'https://cdn.midjourney.com/c760a5d6-2b81-4a33-bfa7-ac9720231f15/0_2_384_N.webp',
-  'https://cdn.midjourney.com/70f3e0f2-a079-45ef-a168-474b0d8762dc/0_3_384_N.webp',
-  'https://cdn.midjourney.com/ed4abd0a-b76b-4a3a-b52e-56edf5d3ab2a/0_1.png',
-  'https://cdn.midjourney.com/db651aef-0408-4928-842c-0f1ecd1787b5/0_3_384_N.webp',
+  firstScreenshot,
+  secondScreenshot,
+  thirdScreenshot,
+  fourthScreenshot,
+  fifthScreenshot,
+  sixthScreenshot,
+  seventhScreenshot,
+  eighthScreenshot,
+  ninethScreenshot,
 ];
 
 export const ImageCarousel = () => {
   return (
-    <div className="infinite-carousel">
-      <Marquee gradient={false} speed={50} autoFill={true}> {/* Adjust the speed as needed */}
+    <div className="infinite-carousel h-auto">
+      <Marquee gradient={false} speed={50} autoFill={true}>
         {images.map((image, index) => (
-          <img key={index} src={image} alt={`Image ${index}`} />
+          <img key={index} src={image} alt={`Image ${index}`}/>
         ))}
       </Marquee>
     </div>
--- Landing/src/page/Home.jsx
+++ Landing/src/page/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
 
         {/* Make this into infinite scrolling component */}
         <section id="img-carousel" className="w-screen h-[150px] bg-transparent 2xl:-translate-y-44 xl:-translate-y-16 lg:-translate-y-20 sm:-translate-y-20 -translate-y-12 z-10">
-            <div className="flex absolute bg-white w-[200%] -translate-x-4 2xl:h-[200px] h-[175px] -rotate-3 overflow-hidden">
+            <div className="flex absolute bg-gradient-to-r from-[#FFFFFF] to-[#AF0F5C] w-[200%] -translate-x-4 2xl:h-[200px] h-[175px] -rotate-3 overflow-hidden object-contain">
                 <ImageCarousel />
             </div>
         </section>
@@ -22,9 +22,10 @@ const Home = () => {
         {/* Footbrawl area, transparent with our logo splash art as the main bg*/}
         <section id="footbrawl" className="flex flex-col w-screen h-auto justify-start items-center 2xl:-mt-32 xl:-mt-12 lg:-mt-16 sm:-mt-20 -mt-28 gap-y-4 px-4">
             <div id="footbrawl-header" className="flex flex-col items-center justify-center">
-                <img id="x" src="./src/assets/characters/X.png" className="w-48 h-48" />
+                <img id="x" src="./src/assets/characters/X.png" className="2xl:w-96  -translate-x-[5px] 2xl:h-96 xl:w-64 xl:h-64 w-48 h-48 grayscale opacity-50" />
+                <img id="x" src="./src/assets/characters/X.png" className="absolute -translate-y-6 2xl:w-96 2xl:h-96 xl:w-64 xl:h-64 w-48 h-48" />
                 <div>
-                    <h2 className="font-black lg:text-[70px] md:text-[60px] sm:text-5xl text-3xl bg-gradient-to-b from-[#E01377] to-[#AF0F5C] bg-clip-text text-transparent uppercase drop-shadow-md pb-4">Join the footbrawl!</h2>
+                    <h2 id="footbrawl-text" className="font-black lg:text-[70px] md:text-[60px] sm:text-5xl text-3xl bg-gradient-to-b from-[#E01377] to-[#AF0F5C] bg-clip-text text-transparent uppercase drop-shadow-md pb-4">Join the footbrawl!</h2>
                 </div>
             </div>
             <div id="trailer-video" className="flex justify-center items-center lg:max-w-[800px] lg:w-[800px] md:max-w-[650px] max-w-[375px]">
@@ -47,12 +48,12 @@ const Home = () => {
                 />
             </div>
             <div id="download-links" className="flex flex-wrap justify-center items-center w-full h-auto py-2 lg:pb-16 pb-6">
-                <a href=""><img src="./src/assets/applebutton.png" className="sm:max-w-[250px] max-w-[300px]"/></a>
-                <a href=""><img src="./src/assets/googlebutton.png" className="sm:max-w-[250px] max-w-[300px]"/></a>
-                <a href=""><img src="./src/assets/nintendobutton.png" className="sm:max-w-[250px] max-w-[300px]"/></a>
-                <a href=""><img src="./src/assets/playstationbutton.png" className="sm:max-w-[250px] max-w-[300px]"/></a>
-                <a href=""><img src="./src/assets/steambutton.png" className="sm:max-w-[250px] max-w-[300px]"/></a>
-                <a href=""><img src="./src/assets/xboxbutton.png" className="sm:max-w-[250px] max-w-[300px]"/></a>
+                <a href=""><img src="./src/assets/applebutton.png" className="sm:max-w-[250px] max-w-[300px] hover:scale-110 transition-all duration-100 ease-in-out"/></a>
+                <a href=""><img src="./src/assets/googlebutton.png" className="sm:max-w-[250px] max-w-[300px] hover:scale-110 transition-all duration-100 ease-in-out"/></a>
+                <a href=""><img src="./src/assets/nintendobutton.png" className="sm:max-w-[250px] max-w-[300px] hover:scale-110 transition-all duration-100 ease-in-out"/></a>
+                <a href=""><img src="./src/assets/playstationbutton.png" className="sm:max-w-[250px] max-w-[300px] hover:scale-110 transition-all duration-100 ease-in-out"/></a>
+                <a href=""><img src="./src/assets/steambutton.png" className="sm:max-w-[250px] max-w-[300px] hover:scale-110 transition-all duration-100 ease-in-out"/></a>
+                <a href=""><img src="./src/assets/xboxbutton.png" className="sm:max-w-[250px] max-w-[300px] hover:scale-110 transition-all duration-100 ease-in-out"/></a>
             </div>
         </section>
         
@@ -61,9 +62,9 @@ const Home = () => {
 
         <CharacterInteractive />
 
-        <img id="second-divider" src="./src/assets/pagedivider.png" className="absolute left-0 -translate-y-[5px] w-screen h-[40px]" />
+        <img id="second-divider" src="./src/assets/pagedivider.png" className="absolute left-0 -translate-y-[5px] w-screen h-fit" />
         {/* Add scalability for scrolling to see more skins */}
-        <section id="skin-showcase" className="w-screen h-auto overflow-y-hidden overflow-x-hidden">
+        <section id="skin-showcase" className="2xl:mt-2 w-screen h-auto overflow-y-hidden overflow-x-hidden">
             <div id="skin-gradient" className="absolute w-full h-[400px]" />
             <div id="skins" className="flex items-center justify-center w-full h-[400px] bg-[#131313] gap-x-[5px]">
                 <div id="skin" className="w-[100px] h-[408px] rotate-3 bg-black overflow-hidden">
@@ -134,8 +135,55 @@ const Home = () => {
                 <p className="font-medium text-[20px] text-slate-500">With plenty of Emotes, Titles, and Skins</p>
             </div>
         </section>
-        <section id="customization" className="w-screen h-[600px] bg-[#131313] py-8">
+        <section id="customization" className="flex justify-center w-screen h-auto bg-[#131313] px-8 pt-36 overflow-hidden">
             {/* Section to show off Emotes, Titles, and Nameplates  */}
+            <div id="customization-wrapper" className="flex flex-wrap items-center justify-evenly w-full max-w-[1280px] h-full xl:gap-x-16 gap-y-12 py-8">
+                <div id="customization-module" className="flex lg:flex-row flex-col justify-center items-center w-full xl:w-[400px] lg:w-[600px] h-auto ">
+                    <div id="module-text" className="flex flex-col lg:text-start text-center w-full h-auto p-4">
+                        <h2 className="font-black text-[36px] tracking-wide uppercase text-[#FDF0EE]">Skins</h2>
+                        <p className="text-slate-500 text-[18px] xl:text-[14px] ">Customize your character's appearance and showcase your unique style!</p>
+                    </div>
+                    <div id="module-img" className="">
+                        <img src="./src/assets/skins/SkinsPack.png" />
+                    </div>
+                </div>
+                <div id="customization-module" className="flex lg:flex-row flex-col justify-center items-center w-full xl:w-[400px] lg:w-[600px] h-auto ">
+                    <div id="module-text" className="flex flex-col lg:text-start text-center w-full h-auto p-4">
+                        <h2 className="font-black text-[36px] tracking-wide uppercase text-[#FDF0EE]">Titles</h2>
+                        <p className="text-slate-500 text-[18px] xl:text-[14px] ">Elevate your in-game status with exclusive titles.</p>
+                    </div>
+                    <div id="module-img" className="">
+                        <img src="./src/assets/TitleStack.png" />
+                    </div>
+                </div>
+                <div id="customization-module" className="flex lg:flex-row flex-col justify-center items-center w-full xl:w-[400px] lg:w-[600px] h-auto ">
+                    <div id="module-text" className="flex flex-col lg:text-start text-center w-full h-auto p-4">
+                        <h2 className="font-black text-[36px] tracking-wide uppercase text-[#FDF0EE]">Emotes</h2>
+                        <p className="text-slate-500 text-[18px] xl:text-[14px] ">Express yourself with fun emotes!</p>
+                    </div>
+                    <div id="module-img" className="">
+                        <img src="./src/assets/EmoticonStack.png" />
+                    </div>
+                </div>
+                <div id="customization-module" className="flex lg:flex-row flex-col justify-center items-center w-full xl:w-[400px] lg:w-[600px] h-auto">
+                    <div id="module-text" className="flex flex-col lg:text-start text-center w-full h-auto p-4">
+                        <h2 className="font-black text-[36px] tracking-wide uppercase text-[#FDF0EE]">Goal Explosions</h2>
+                        <p className="text-slate-500 text-[18px] xl:text-[14px] ">Make every goal a memorable spectacle.</p>
+                    </div>
+                    <div id="module-img" className="">
+                        <img src="./src/assets/ExplosionStack.png" />
+                    </div>
+                </div>
+                <div id="customization-module" className="flex lg:flex-row flex-col justify-center items-center w-full xl:w-[400px] lg:w-[600px] h-auto">
+                    <div id="module-text" className="flex flex-col lg:text-start text-center w-full h-auto p-4">
+                        <h2 className="font-black text-[36px] tracking-wide uppercase text-[#FDF0EE]">Battlepass</h2>
+                        <p className="text-slate-500 text-[18px] xl:text-[14px] ">Unlock exclusive content, challenges, and rewards seasonally!</p>
+                    </div>
+                    <div id="module-img" className="">
+                        <img src="./src/assets/BattlePass.png" />
+                    </div>
+                </div>
+            </div>
         </section>
         <section id="maps">
             {/* Gif of Kill on Ai-Mi map */}
